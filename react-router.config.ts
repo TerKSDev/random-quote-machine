21698e5a7e9300772271f5fad2cdf33150b900b0
HuoYan1213 <huoyan0928@gmail.com>
2026-06-17 13:13:50 +0800

    build: configure SPA mode for GitHub Pages deployment and add gh-pages script

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  basename: "/random-quote-machine/",
   future: {
     v8_middleware: true,
     v8_passThroughRequests: true,
